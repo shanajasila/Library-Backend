ee76f2de8dc8caedbd66614ee04f075e6aca66ed
shanajasila <shanajasilam@gmail.com>
2022-12-05 18:35:09 +0530

    initial

---
+++ src/test/java/com/example/LibraryApp/backend/LibraryAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LibraryApp.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
