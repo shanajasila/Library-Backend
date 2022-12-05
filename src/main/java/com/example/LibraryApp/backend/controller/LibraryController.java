0ad5a389171ebcec1b80878e6fd4b0b686a06444
shanajasila <shanajasilam@gmail.com>
2022-12-05 18:38:56 +0530

    signin

--- src/main/java/com/example/LibraryApp/backend/controller/LibraryController.java
+++ src/main/java/com/example/LibraryApp/backend/controller/LibraryController.java
@@ -1,9 +1,14 @@
 package com.example.LibraryApp.backend.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 
 public class LibraryController {
 
+    @PostMapping("/sighnin")
+    public String sighin(){
+        return "welcome to sign in page";
+    }
 }
